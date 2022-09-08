08727980b550157fc91000c0287f4fa7ea2013ff
haleont <60760760+haleont@users.noreply.github.com>
2022-09-08 10:34:53 -0500

    Update main.js

--- tools/toolGGBFrame/js/main.js
+++ tools/toolGGBFrame/js/main.js
@@ -3,8 +3,8 @@ window.addEventListener("load", main);
 function main() {
     let $parameters = {
             "id": "g7kcfcqg",
-            "width": 672,
-            "height": 378,
+            "width": 624,
+            "height": 351,
             "border": "ffffff",
             "rc": false,
             "ai": false,
