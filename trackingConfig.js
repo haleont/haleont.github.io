14c96b5370e3a281459f76819691d1de10abaa1d
haleont <60760760+haleont@users.noreply.github.com>
2025-05-23 15:07:02 -0500

    Add files via upload

---
+++ trackingConfig.js
@@ -0,0 +1,10 @@
+var TrackingConfig = {
+    "success_status": "passed",
+    "unsuccess_status": "failed",
+    "send_score": "true",
+    "send_status": "true",
+    "send_interactions": "true",
+    "send_objectives": "true",
+    "mastery_score": "80",
+    "type": "none"
+}
